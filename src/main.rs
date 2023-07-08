c16b21d37c5a92f64b4a1f8da68537c733f33e1f
Sarek <sarek@uliweb.de>
2023-07-08 23:37:56 +0200

    Allow uploading new data
    Create a landing page that offers an option to upload new data. On this
    new page, have a form that allows choosing a file to upload.
    
    Once the file is uploaded, store it, and display a helpful "Done"
    page that also states the URL that is to be used for downloads in the
    future.
    
    For customization of the "Done" page, for now we have an extremely
    simple "templating engine" that just use string replacements to replace
    some placeholders. In the future this should probably be replaced by
    something more elaborate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get_service,
+    Router,
+    Server, extract::DefaultBodyLimit,
+};
+use tower_http::services::ServeFile;
+use simple_logger::SimpleLogger;
+
+#[macro_use]
+extern crate log;
+
+mod share;
+// mod retrieve;
+
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new().init().unwrap();
+    
+    let index = ServeFile::new("assets/index.html");
+    let new_static = ServeFile::new("assets/new.html");
+
+    let app = Router::new()
+        .route("/new", get_service(new_static).post(share::share_handler))
+        .route_service("/", index)
+        .layer(DefaultBodyLimit::max(1024 * 1024 * 1024));
+
+    info!("Teimeida starting to serve on port 8080");
+    Server::bind(&([0, 0, 0, 0], 8080).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
---
+++ src/share.rs
@@ -0,0 +1,53 @@
+use axum::{extract::Multipart, response::Html};
+use nanoid::nanoid;
+use std::{fs::{File, self}, io::Write};
+
+pub async fn share_handler(mut multipart: Multipart) -> Html<String> {
+    let id = nanoid!();
+    let storage_path = "data/".to_owned() + &id;
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        if field.name().unwrap() == "data" {
+            if let Ok(mut file) = File::create(&storage_path) {
+                let orig_name = field.file_name().unwrap().to_string();
+                let data = field.bytes().await.unwrap();
+
+                if let Ok(_) = file.write_all(&data) {
+                    // save meta-data as extended attributes
+                    let _ = xattr::set(
+                        storage_path,
+                        "user.teimeida.orig_name",
+                        &orig_name.as_bytes(),
+                    );
+                    return create_success_response(&orig_name, &data.len().to_string(), &id).await;
+                } else {
+                    return create_error_response("Could not save all uploaded data").await;
+                }
+
+            } else {
+                return create_error_response("Could not create storage file").await;
+            }
+        }
+    }
+    create_error_response("No uploaded data found").await
+}
+
+pub async fn create_success_response(filename: &String, size: &String, id: &String) -> Html<String> {
+    if let Ok(template) = fs::read_to_string("assets/upload_done.tpl.html") {
+        let final_document = template
+            .replace("%filename%", filename)
+            .replace("%size%", size)
+            .replace("%id%", id);
+        return Html(final_document);
+    } else {
+        // fail
+    }
+    Html("".to_string())
+
+}
+
+pub async fn create_error_response(msg: &str) -> Html<String> {
+    let mut retval = String::from("<html><head><title>Teimeida Error</title></head><body><h1>Internal Server Error</h1><p>");
+    retval.push_str(msg);
+    retval.push_str("</p></body></html>");
+    Html(retval)
+}
